a270c2f0e66aa2b00b2592c1a9c2c1140e40f22f
Jesse de Zwart <jessedezwart@live.nl>
2022-10-12 18:29:28 +0200

    Add Spring Boot files

---
+++ src/main/java/org/spine/iquestionapi/IQuestionApiApplication.java
@@ -0,0 +1,13 @@
+package org.spine.iquestionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IQuestionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IQuestionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spine/iquestionapi/IQuestionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spine.iquestionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IQuestionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
