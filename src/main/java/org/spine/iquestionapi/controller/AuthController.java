a0b9d4b10fba1cdde5d270ba4bb64045c6e6a647
Jesse de Zwart <jessedezwart@live.nl>
2022-10-17 12:19:50 +0200

    fix: update api paths

--- src/main/java/org/spine/iquestionapi/controller/AuthController.java
+++ src/main/java/org/spine/iquestionapi/controller/AuthController.java
@@ -18,7 +18,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
 
     @Autowired private UserRepo userRepo;
--- src/main/java/org/spine/iquestionapi/controller/UserController.java
+++ src/main/java/org/spine/iquestionapi/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired private UserRepo userRepo;
