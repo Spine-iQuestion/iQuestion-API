12156050bdac640dfaf2d66af0ad481f559b0e83
Jesse de Zwart <jessedezwart@live.nl>
2022-10-22 17:26:45 +0200

    fix: add domain check for email validation

--- src/main/java/org/spine/iquestionapi/util/StringUtil.java
+++ src/main/java/org/spine/iquestionapi/util/StringUtil.java
@@ -12,7 +12,7 @@ public class StringUtil {
     }
 
     public static boolean isValidEmail(String email){
-        return email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+        return email.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
     }
 
     public static boolean isSafePassword(String password) {
