c974b8bb0eab0af2205597fe7fa079dc7b4ce08f
Jesse de Zwart <jessedezwart@live.nl>
2022-11-27 11:48:41 +0100

    fix: update dependency with vuln

--- src/main/java/org/spine/iquestionapi/IQuestionApiApplication.java
+++ src/main/java/org/spine/iquestionapi/IQuestionApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -25,9 +26,12 @@ public class IQuestionApiApplication {
 	}
 
 	@Bean
-	public Docket productApi() {
-	   return new Docket(DocumentationType.SWAGGER_2).select()
-		  .apis(RequestHandlerSelectors.basePackage("org.spine.iquestionapi")).build();
-	}
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
