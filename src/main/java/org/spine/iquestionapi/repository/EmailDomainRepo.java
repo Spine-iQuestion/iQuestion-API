b247c2acb16d22c2e5a7df554a743804a9de1cfa
Jesse de Zwart <jessedezwart@live.nl>
2023-01-08 15:37:34 +0100

    Feat: add add-user option

--- src/main/java/org/spine/iquestionapi/repository/EmailDomainRepo.java
+++ src/main/java/org/spine/iquestionapi/repository/EmailDomainRepo.java
@@ -1,11 +1,13 @@
 package org.spine.iquestionapi.repository;
 
+import java.util.Optional;
+
 import org.spine.iquestionapi.model.EmailDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * The repository for the EmailDomain
  */
-public interface EmailDomainRepo extends JpaRepository<EmailDomain, Long> {
-
+public interface EmailDomainRepo extends JpaRepository<EmailDomain, String> {
+    Optional<EmailDomain> findByDomain(String domain);
 }
